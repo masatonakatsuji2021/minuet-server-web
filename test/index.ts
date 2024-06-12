52d37061742e3379855c8dbf2a4be9fcd6918500
satou akira <nakatsuji@teastalk.jp>
2024-06-12 11:47:21 +0900

    6/12

--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,23 @@
+// Simple Server Test Sample
 import { MinuetWeb } from "../";
 import * as http from "http";
 
 const mw = new MinuetWeb({
+    // root directory
+    rootDir: "htdocs",
+    // 404 not found HTML file.
     notFound:  "error.html",
+    // response header 
     headers : {
+        // cache control (keep max 60s)
         "cache-control":"max-age=60",
     }
 });
 
+// server listen
 const h = http.createServer((req, res)=>{
+    // static content listen
     const status = mw.listen(req, res);
 });
 h.listen(1505);
-
 console.log("Listen http://localhost:1505");
